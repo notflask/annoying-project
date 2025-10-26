9100714c0cc45962973761be6e95678ad3794018
flask <notflask@proton.me>
2025-10-26 11:22:51 +0100

    remove comma after list, so it treats it like list

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,14 +5,12 @@ class Settings(BaseSettings):
     app_name: str = "Annoying Project"
     debug: bool = True
     database_url: str = "sqlite:///./database.db"
-    cors_origins: list[str] = (
-        [
-            "http://localhost:5173",
-            "http://localhost:3000",
-            "http://127.0.0.1:5173",
-            "http://127.0.0.1:3000",
-        ],
-    )
+    cors_origins: list[str] = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
     static_dir: str = "static"
     images_dir: str = "static/images"
 
